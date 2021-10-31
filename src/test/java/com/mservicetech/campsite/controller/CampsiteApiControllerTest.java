7d4f7c207011f0f87c7ae62b4dca572d37730740
Gavin Chen <gavinyan.han@gmail.com>
2021-10-30 21:06:51 -0400

    fix the h2 test data issue

--- src/test/java/com/mservicetech/campsite/controller/CampsiteApiControllerTest.java
+++ src/test/java/com/mservicetech/campsite/controller/CampsiteApiControllerTest.java
@@ -21,7 +21,6 @@ import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@Disabled
 @SpringBootTest
 @AutoConfigureMockMvc
 class CampsiteApiControllerTest {
